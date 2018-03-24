2e2c66e6c543341d22648374f3df6846e13f3ced
Begbeder <safonov.front@gmail.com>
2018-03-24 23:08:25 +0700

    add crud

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const config = require('./config')
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // moq
-const artists = [
+let artists = [
     {
         id: 1,
         name: 'name1'
@@ -32,6 +36,31 @@ app.get('/artists/:id', (req, res) => {
     const artist = artists.find(artist => artist.id === Number(req.params.id))
     
     res.send(artist)
+})
+
+app.post('/artists', (req, res) => {
+    const artist = {
+        id: Date.now(),
+        name: req.body.name
+    }
+
+    artists.push(artist)
+
+    res.send(artist)
+})
+
+app.put('/artists/:id', (req, res) => {
+    const artist = artists.find(artist => artist.id === Number(req.params.id))
+    
+    artist.name = req.body.name
+
+    res.sendStatus(200)
+})
+
+app.delete('/artists/:id', (req, res) => {
+    artists = artists.filter(artist => artist.id !== Number(req.params.id))
+
+    res.sendStatus(200)
 })
 
 app.listen(config.port, () => {
